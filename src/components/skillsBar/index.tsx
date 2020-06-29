44305a0bf5b7f0b5ec38c40d85e8b4a3cfddb64b
Olga Serebryakova <lelka-7770@yandex.ru>
2020-06-29 13:33:21 +0300

    Change color skills bar

--- src/components/skillsBar/index.tsx
+++ src/components/skillsBar/index.tsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {isPartiallyVisible} from "../showUpAnimation";
 
 import {
-  Wrapper
+  ProgressContainer,
+  ProgressTitle,
+  ProgressString,
+  ProgressSpan
 } from './style';
+
 
 interface IProps {
   theme: string,
-  data: object
+  skill: ISkillItem
 }
 
+interface ISkillItem {
+  type: string,
+  scale: number
+}
 
-const Diagram:React.FC<IProps> = () => {
+const Progress:React.FC<IProps> = (props) => {
+
+  const [isAnimateProgress, setAnimateProgress]  = useState(false);
+  const elRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const elementRefCurrent = elRef.current;
+
+      if (elementRefCurrent && isPartiallyVisible(elementRefCurrent)) {
+        setAnimateProgress(true);
+      } else {
+        setAnimateProgress(false);
+      }
+    }
+    window.addEventListener('scroll', onScroll )
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  })
+
   return(
-    <Wrapper>
-
-    </Wrapper>
+    <ProgressContainer ref={elRef}>
+      <ProgressTitle theme={props.theme}>{props.skill.type}</ProgressTitle>
+      <ProgressString theme={props.theme} >
+        <ProgressSpan theme={props.theme} scale={props.skill.scale} isAnimateProgress={isAnimateProgress}/>
+      </ProgressString>
+    </ProgressContainer>
   )
 }
 
-export default Diagram;
+export default Progress;
--- src/components/skillsBar/style.ts
+++ src/components/skillsBar/style.ts
@@ -1,11 +1,71 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
-export const Wrapper = styled.section`
-  width: 100%;
+export const ProgressContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  margin: 10px;
+  grid-template-columns: 1fr 2fr;
+`;
+
+export const ProgressTitle = styled.div`
+  color: ${props => props.theme === 'light' ? '#767777' : '#fff'};;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  top: 1px;
+  height: 60px;
+  z-index: 3;
+  padding-right: 20px;
+`;
+
+export const ProgressString = styled.div`
+  height: 60x;
+  position: relative;
+  vertical-align: top;
+  z-index: 0;
+  top: 3px;
+  margin-right: 10px;
+  
+  :before {
+    content: '';
+    background-color: #ddd;
+    height: 10px;
+    border-radius: 5px;
+    content: '';
+    display: block;
+    height: 10px;
+    position: absolute;
+    width: 100%;
+    z-index: -1;
+  }
+  
+`;
+
+function progressBuilder(scale: number) {
+  const progress = keyframes`
+      0% {
+        width: 0%;
+      }
+      100% {
+        width: ${scale} + '%';
+      }
+  `;
+  return progress
+}
+
+
+interface IProgressString {
+  scale: number,
+  theme: string,
+  isAnimateProgress: boolean
+}
+
+export const ProgressSpan = styled.div<IProgressString>`
+  width: ${props => props.scale+'%'};
+  background-color: ${props => props.theme === 'light' ? '#A493DF' : '#C6FEFF'};
+  height: 10px;
+  border-radius: 5px;
+  z-index: 10;
+  vertical-align: top;
+  position: absolute;
+  animation: ${props => props.isAnimateProgress ? progressBuilder(props.scale) : ''} 0.5s linear ;
 `;
 
 
